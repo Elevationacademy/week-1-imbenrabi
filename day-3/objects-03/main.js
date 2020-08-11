6d0ed89295761a3cf2e3f2dc81a242581f3a8c9d
imbenrabi <imbenrabi@gmail.com>
2020-08-11 16:30:22 +0300

    adding ex object-5.2

--- day-3/objects-03/main.js
+++ day-3/objects-03/main.js
@@ -1,11 +1,18 @@
+//EX 05 - .2
 const reservations = {
     Bob: { claimed: false },
     Ted: { claimed: true }
 }
 
+//just adjusting our data base a bit to apply with our new 5.2 feature
+reservations.bob = { claimed: false };
+reservations.ted = { claimed: true };
+delete reservations.Bob;
+delete reservations.Ted;
 
-const name = prompt('Please enter the name for your reservation');
+const name = prompt('Please enter the name for your reservation').toLowerCase();
 // console.log('1', reservations[name])
+
 
 if (!reservations[name]) {
     reservations[name] = {
@@ -18,3 +25,5 @@ if (!reservations[name]) {
 }
 
 // console.log('2', reservations[name])
+
+
